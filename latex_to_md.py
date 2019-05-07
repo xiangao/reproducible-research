caf3b459fd81a07528dd3eeec5b1251cb6e6bcb1
Andrew Marder <andrew.n.marder@gmail.com>
2019-05-07 09:10:06 -0400

    Adding a whole bunch of replacements

--- latex_to_md.py
+++ latex_to_md.py
@@ -5,8 +5,16 @@ import re
 
 def clean(text):
     l = [
+        (r'\\[hv]space\{[^}]+\}', ''),
         (r'\{\\hilit ([^}]+)\}', lambda m: '**%s**' % m.group(1)),
+        (r'\\hilit', ''),
+        (r'\{\\lolit ([^}]+)\}', lambda m: m.group(1)),
+        (r'\{\\tt ([^}]+)\}', lambda m: '`%s`' % m.group(1)),
         (r'\\href\{([^}]+)\}\{([^}]+)\}', lambda m: '[%s](%s)' % (m.group(2), m.group(1))),
+        (r'\\textasciitilde', '~'),
+        (r'\\texttt{([^}]+)}', lambda m: '`%s`' % m.group(1)),
+        (r'\{\\footnotesize \\lolit ([^}]+)\}', lambda m: m.group(1)),
+        (r'\\(bbi|bi|ei)\b\s*', ''),
     ]
     result = text
     for pattern, repl in l:
@@ -18,6 +26,12 @@ def test_clean():
     pairs = [
         (r'{\hilit Real}', '**Real**'),
         (r'\href{http://example.org}{example}', '[example](http://example.org)'),
+        (r'{\tt git status}', '`git status`'),
+        (r'\textasciitilde', '~'),
+        (r'\texttt{m}', '`m`'),
+        (r'{\footnotesize \lolit hi}', 'hi'),
+        (r'\hspace{1in}', ''),
+        (r'\bbi', ''),
     ]
     for in_text, out_text in pairs:
         assert clean(in_text) == out_text
